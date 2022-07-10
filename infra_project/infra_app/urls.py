2af53b9168521f2c324f0afc9aadcbaf1b7ca41e
Илья Симонов <ilya.shinoda@yandex.ru>
2022-07-10 23:51:19 +0300

    add deploy to workflow fixed 3

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -7,5 +7,5 @@ app_name = 'infra_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('second_page/', views.second_page, name='second_page'),
-    # path('second_page2/', views.second_page, name='second_page'),
+    path('second_page2/', views.second_page, name='second_page'),
 ]
